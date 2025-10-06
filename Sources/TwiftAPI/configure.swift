3b9491c2cb6a75b15194c417df014a2f0c787337
Valentin Lehmann <info@valentinlehmann.de>
2025-10-07 00:03:32 +0200

    Add CORS middleware

--- Sources/TwiftAPI/configure.swift
+++ Sources/TwiftAPI/configure.swift
@@ -1,9 +1,17 @@
 import Vapor
 
+let corsConfiguration = CORSMiddleware.Configuration(
+    allowedOrigin: .all,
+    allowedMethods: [.GET],
+    allowedHeaders: [.authorization, .origin, .accessControlAllowOrigin, .contentType, .accept]
+)
+
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    
+    app.middleware.use(CORSMiddleware(configuration: corsConfiguration), at: .beginning)
 
     // register routes
     try routes(app)
