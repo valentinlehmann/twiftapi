a0f0b7aef0541b5fd770a9f4ba2ac745ad7b8487
Valentin Lehmann <info@valentinlehmann.de>
2025-10-07 00:14:31 +0200

    Fix code style issues

--- Sources/TwiftAPI/configure.swift
+++ Sources/TwiftAPI/configure.swift
@@ -10,7 +10,7 @@ let corsConfiguration = CORSMiddleware.Configuration(
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-    
+
     app.middleware.use(CORSMiddleware(configuration: corsConfiguration), at: .beginning)
 
     // register routes
--- Sources/TwiftAPI/routes.swift
+++ Sources/TwiftAPI/routes.swift
@@ -4,41 +4,48 @@ let token = ProcessInfo.processInfo.environment["TWITCH_API_TOKEN"]!
 let clientId = ProcessInfo.processInfo.environment["TWITCH_CLIENT_ID"]!
 
 struct Wrapper: Codable {
-    var data: [Stream]
+    let data: [Stream]
 }
 
 struct Stream: Codable {
-    var id: String
-    var user_id: String
-    var user_login: String
-    var user_name: String
-    var viewer_count: Int
+    let id: String
+    let userId: String
+    let userLogin: String
+    let userName: String
+    let viewerCount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case userId = "user_id"
+        case userLogin = "user_login"
+        case userName = "user_name"
+        case viewerCount = "viewer_count"
+    }
 }
 
 func routes(_ app: Application) throws {
-    app.get { req async in
+    app.get { _ async in
         "It works!"
     }
-    
+
     app.get("viewcount", ":login") { req async throws -> Int in
         let login = try req.parameters.require("login")
-        
+
         let response = try await req.client.get(URI(string: "https://api.twitch.tv/helix/streams")) { req in
             try req.query.encode(["user_login": login, "first": "1"])
-            
+
             let auth = BearerAuthorization(token: token)
             req.headers.bearerAuthorization = auth
-            
+
             req.headers.add(name: "Client-Id", value: clientId)
         }
-        
+
         let wrapper = try response.content.decode(Wrapper.self)
-        
-        if (wrapper.data.isEmpty) {
+
+        if wrapper.data.isEmpty {
             return -1
         }
-        
-        return wrapper.data[0].viewer_count
+
+        return wrapper.data[0].viewerCount
     }
 }
-
